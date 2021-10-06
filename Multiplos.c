e2125318980f3ae610f24938ace8efe65c2c3a03
jorgealexis07 <78820449+jorgealexis07@users.noreply.github.com>
2021-10-06 13:01:32 -0500

    Create Multiplos.c

---
+++ Multiplos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+  int i,a,fact=1;
+  printf("Numeros multiplos entre 10 a 50\n");
+  for(i=10;i<=50;i=i+5)
+  {
+  	  printf("\n%d\n",i);
+  	  	fact=fact*i;
+  	  	
+  	  	 printf("su factorial es:\n%d"),fact;
+	  }
+}
+ 
+ 
